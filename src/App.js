bb28da0240c6274005582e6c81dab2f2ff412445
Lee-d0 <marmar11137@gmail.com>
2024-07-31 11:02:25 -0400

    React-Library

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./componets/Nav.jsx"
 
+import Footer from "./componets/Footer.jsx";
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Home from "./pages/Home.jsx";
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route />
+      <Nav />
+      <Route path="/" exact component={Home} />
+      
+      <Footer />
     </div>
+    </Router>
   );
 }
 
