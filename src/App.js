e76ca67460b6e7ef31918511e9097a37f583e1f3
Lee-d0 <marmar11137@gmail.com>
2024-08-01 16:06:41 -0400

    update 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import Nav from "./componets/Nav.jsx"
 
 import Footer from "./componets/Footer.jsx";
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route} from "react-router-dom"
 import Home from "./pages/Home.jsx";
+import Books from "./pages/Books.jsx";
+import {books} from "./data"
+import BookInfo from "./pages/BookInfo.jsx";
+import Cart from "./pages/Cart.jsx";
+import React , { useEffect, useState } from "react";
 function App() {
+  const [cart, setCart] = useState([])
+  function addToCart(book){
+    setCart([...cart, book])
+  }
+
+  useEffect(() =>{
+    console.log(cart)
+  },[cart])
+
   return (
     <Router>
-    <div className="App">
-      <Route />
-      <Nav />
-      <Route path="/" exact component={Home} />
-      
-      <Footer />
-    </div>
+      <div className="App">
+        
+        <Nav />
+        <Route exact path="/"  component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />}/>
+        <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart} cart={cart}/>}/>
+        <Route path="/cart" render={() => <Cart books={books}/> }/>
+        <Footer />
+      </div>
     </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom/client';
 import "./index.css"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt  } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft  } from '@fortawesome/free-solid-svg-icons'
 
 
-library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt   )
+library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft   )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
